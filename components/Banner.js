bf95dc6d082faa961e54970093805b75fd7dfbd0
Ben Elferink <ben.elferink@icloud.com>
2021-06-04 11:57:42 +0300

    added some features for homescreen

--- components/Banner.js
+++ components/Banner.js
@@ -4,6 +4,10 @@ const h2Text = "Anonymous Voice Protocol";
 const h3Text = "Gain +Karma by helping other people";
 
 export default function Banner() {
+  const clickLaunch = () => {
+    alert("Under Development");
+  };
+
   return (
     <div className={`flex-column ${styles.container}`}>
       <h2 className={styles.glitch}>
@@ -16,6 +20,10 @@ export default function Banner() {
         {h3Text}
         <span aria-hidden='true'>{h3Text}</span>
       </h3>
+
+      <button className={`${styles.glitch} ${styles.launch}`} onClick={clickLaunch}>
+        Launch App
+      </button>
     </div>
   );
 }
--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,20 @@
+import { useRouter } from "next/router";
+import MicSVG from "./MicSVG";
 import styles from "../styles/Header.module.css";
 
 export default function Header() {
-  return <header className={`flex-row ${styles.container}`}>Karmic</header>;
+  const router = useRouter();
+
+  const clickLogo = () => {
+    router.push("/");
+  };
+
+  return (
+    <header className={`flex-row ${styles.container}`}>
+      <div className={`flex-row ${styles.logo}`} onClick={clickLogo}>
+        <MicSVG size='22' />
+        <h1>Karmic</h1>
+      </div>
+    </header>
+  );
 }
