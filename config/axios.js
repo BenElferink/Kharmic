7eb09f83dd6d7fa53904fd8e39ae7d5b8a1ae304
Ben Elferink <ben.elferink@icloud.com>
2021-06-10 16:06:12 +0300

    remove base url

--- config/axios.js
+++ config/axios.js
@@ -3,7 +3,6 @@ import axios from "axios";
 // create api config (needs the token on most requests)
 const Axios = (token) => {
   return axios.create({
-    baseURL: process.env.API_URI,
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${token}`,
