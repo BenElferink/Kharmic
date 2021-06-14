24f55dbe4ba1e8b60bbfaf95c0c8f1cf2a8da8ba
Ben Elferink <ben.elferink@icloud.com>
2021-06-14 19:39:59 +0300

    cleanup

--- models/Account.js
+++ models/Account.js
@@ -3,27 +3,11 @@ const { Schema, model, models } = mongoose;
 
 const Account = new Schema(
   {
-    display_name: {
-      type: String,
-      required: true,
-    },
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    passwordHash: {
-      type: String,
-      required: true,
-    },
-    profession: {
-      type: String,
-      default: "Anonymous",
-    },
-    karma: {
-      type: Number,
-      default: 0,
-    },
+    display_name: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    passwordHash: { type: String, required: true },
+    profession: { type: String, default: "Anonymous" },
+    karma: { type: Number, default: 0 },
   },
   {
     timestamps: true,
