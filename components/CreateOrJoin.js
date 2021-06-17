a2c39eb8f2347ecc2689b3474fc9ca30ca5467ba
Ben Elferink <ben.elferink@icloud.com>
2021-06-17 14:41:22 +0300

    background SVGs

--- components/CreateOrJoin.js
+++ components/CreateOrJoin.js
@@ -1,4 +1,4 @@
-import styles from "../styles/LaunchOrCreate.module.css";
+import styles from "../styles/CreateOrJoin.module.css";
 import btnStyles from "../styles/Buttons.module.css";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -19,28 +19,32 @@ function CreateOrJoin() {
 
   return (
     <section className={styles.container}>
-      <div className={styles.lionArtWrap}>
-        <div className={styles.lionArt}>
-          <LionSVG />
-          <h3>Create a Session</h3>
-          <p>It takes courage to ask others for help</p>
+      <div className={styles.lionBg}>
+        <div className={styles.lionFg}>
+          <div className={styles.lionWrap}>
+            <LionSVG />
+            <h3>Create a Session</h3>
+            <p>It takes courage to ask others for help</p>
 
-          <Button
-            className={btnStyles.blueRadient}
-            style={{ marginTop: "11px" }}
-            onClick={account ? toggleModal : notLoggedIn}>
-            Create
-          </Button>
+            <Button
+              className={btnStyles.blueRadient}
+              style={{ marginTop: "11px" }}
+              onClick={account ? toggleModal : notLoggedIn}>
+              Create
+            </Button>
+          </div>
         </div>
       </div>
 
       {modal && <CreateSessionModal toggleModal={toggleModal} />}
 
-      <div className={styles.owlArtWrap}>
-        <div className={styles.owlArt}>
-          <OwlSVG />
-          <h3>Join a Session</h3>
-          <p>Share your wisdom and help others</p>
+      <div className={styles.owlBg}>
+        <div className={styles.owlFg}>
+          <div className={styles.owlWrap}>
+            <OwlSVG />
+            <h3>Join a Session</h3>
+            <p>Share your wisdom and help others</p>
+          </div>
         </div>
       </div>
     </section>
