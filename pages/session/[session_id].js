1e9f57a954dea1557624962979593364c0ec3bcc
Ben Elferink <ben.elferink@icloud.com>
2021-06-30 20:32:08 +0300

    basic UI structure for actual session

--- pages/session/[session_id].js
+++ pages/session/[session_id].js
@@ -1,10 +1,13 @@
-import { Fragment, useState, useEffect } from "react";
+import btnStyles from "../../styles/Buttons.module.css";
+import { Fragment, useState, useEffect, Children } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useRouter } from "next/router";
 import Axios from "../../config/axios";
 import Head from "next/head";
 import Header from "../../components/Header";
-import { CircularProgress } from "@material-ui/core";
+import { Button, CircularProgress } from "@material-ui/core";
+import LionSVG from "../../icons/LionSVG";
+import OwlSVG from "../../icons/OwlSVG";
 
 export default function SessionPage() {
   const router = useRouter();
@@ -34,7 +37,13 @@ export default function SessionPage() {
     if (session_id) fetchSession();
   }, [session_id, token]);
 
-  console.log(session);
+  const clickTime = () => {
+    dispatch({ type: "TOAST", payload: { txt: "Under development", type: "error" } });
+  };
+
+  const clickTalk = () => {
+    dispatch({ type: "TOAST", payload: { txt: "Under development", type: "error" } });
+  };
 
   return (
     <div className={`page`}>
@@ -50,9 +59,118 @@ export default function SessionPage() {
           <CircularProgress />
         ) : (
           <Fragment>
-            Session ID - {String(session._id)}
-            <br />
-            {session.date_and_time}
+            <div>
+              <h4 style={{ fontSize: "1.5rem", fontWeight: "600" }}>
+                {session.main_category} - {session.sub_category}
+              </h4>
+
+              <div
+                style={{
+                  width: "200px",
+                  height: "200px",
+                  margin: "22px auto",
+                  background: "url('/images/greek_ring.png') no-repeat center",
+                  backgroundSize: "contain",
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  position: "relative",
+                  opacity: "0.5",
+                }}>
+                <LionSVG
+                  size='60'
+                  style={{
+                    position: "absolute",
+                    top: "3%",
+                    left: "3%",
+                    background: "white",
+                    borderRadius: "100%",
+                  }}
+                />
+                <label>Time left</label>
+                <span style={{ fontSize: "3rem", fontWeight: "600" }}>03:54</span>
+              </div>
+
+              <h6 style={{ fontSize: "1rem", fontWeight: "500" }}>CHAPTER I</h6>
+              <h5 style={{ fontSize: "1.1rem", fontWeight: "600" }}>
+                {session.host?.display_name} is talking
+              </h5>
+            </div>
+
+            <div
+              style={{
+                maxWidth: "330px",
+                margin: "22px auto",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}>
+              <Button className={btnStyles.white} onClick={clickTime}>
+                Give time
+              </Button>
+              <Button className={btnStyles.blueRadient} onClick={clickTalk}>
+                Ask to speak
+              </Button>
+            </div>
+
+            <div
+              style={{
+                minHeight: "42vh",
+                background: "var(--gradient-blue-bottom)",
+                color: "var(--beige)",
+                borderRadius: "50% 50% 0 0",
+              }}>
+              <div
+                style={{
+                  maxWidth: "420px",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-evenly",
+                }}>
+                {Children.toArray(
+                  session.participants?.map((partici) => (
+                    <div
+                      style={{
+                        margin: "25px auto 50px auto",
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                      }}>
+                      <OwlSVG
+                        color='black'
+                        size='50'
+                        style={{ background: "white", borderRadius: "100%" }}
+                      />
+                      <span style={{ fontSize: "0.7rem", textShadow: "-1px 1px 1px black" }}>
+                        {partici.display_name}
+                      </span>
+                    </div>
+                  )),
+                )}
+              </div>
+
+              <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                <h6 style={{ marginBottom: "22px" }}>TRANSCRIPT</h6>
+                <ul style={{ textAlign: "left", listStyleType: "none" }}>
+                  <li style={{ marginBottom: "7px", fontSize: "0.8rem", whiteSpace: "nowrap" }}>
+                    <span style={{ marginRight: "11px" }}>05:12</span>{" "}
+                    <span style={{ color: "var(--blue-light)" }}>Test1</span> extended{" "}
+                    <span style={{ color: "var(--blue-light)" }}>Kharmic Developer</span>'s time
+                  </li>
+                  <li style={{ marginBottom: "7px", fontSize: "0.8rem", whiteSpace: "nowrap" }}>
+                    <span style={{ marginRight: "11px" }}>05:12</span>{" "}
+                    <span style={{ color: "var(--blue-light)" }}>Test2</span> asked to speak and is
+                    #1 in queue
+                  </li>
+                  <li style={{ marginBottom: "7px", fontSize: "0.8rem", whiteSpace: "nowrap" }}>
+                    <span style={{ marginRight: "11px" }}>05:12</span>{" "}
+                    <span style={{ color: "var(--blue-light)" }}>Test4</span> extended{" "}
+                    <span style={{ color: "var(--blue-light)" }}>Kharmic Developer</span>'s time
+                  </li>
+                </ul>
+              </div>
+            </div>
           </Fragment>
         )}
       </center>
